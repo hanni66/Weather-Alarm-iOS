203452448337990df093437f243852a6b9084a81
hanni66 <haeunkim0807@naver.com>
2022-07-13 12:36:55 +0900

    Feat : 프로젝트 생성

---
+++ Weather-Alarm/Weather-Alarm/Weather_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AlarmApp.swift
+//  Weather-Alarm
+//
+//  Created by 김하은 on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
