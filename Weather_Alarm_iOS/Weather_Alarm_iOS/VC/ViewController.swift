39e239fedd08d07ba651f077da7f105d2473f991
hanni66 <haeunkim0807@naver.com>
2022-08-19 21:29:28 +0900

    Feat : 뷰에 텍스트 추가

--- Weather_Alarm_iOS/Weather_Alarm_iOS/VC/ViewController.swift
+++ Weather_Alarm_iOS/Weather_Alarm_iOS/VC/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Weather-Alarm
+    lazy var mainText: UILabel = {
+        let mainText = UILabel()
+        mainText.textColor = .black
+        mainText.font = UIFont(name: "NotoSansKR-Bold", size: 20)
+        mainText.font = UIFont.systemFont(ofSize: 30)
+        mainText.text = "Weather-Alarm"
+        mainText.sizeToFit()
+        return mainText
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(self.mainText)
     }
 
 
