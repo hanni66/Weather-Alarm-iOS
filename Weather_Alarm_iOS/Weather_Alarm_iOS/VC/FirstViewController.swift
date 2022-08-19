a70ea6937c42aa65361f6061c6f8e41cda0fc20f
hanni66 <haeunkim0807@naver.com>
2022-08-19 21:12:33 +0900

    Feat : View 생성

---
+++ Weather_Alarm_iOS/Weather_Alarm_iOS/VC/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  Weather_Alarm_iOS
+//
+//  Created by 김하은 on 2022/08/19.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Weather_Alarm_iOS/Weather_Alarm_iOS/VC/SettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewController.swift
+//  Weather_Alarm_iOS
+//
+//  Created by 김하은 on 2022/08/19.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
