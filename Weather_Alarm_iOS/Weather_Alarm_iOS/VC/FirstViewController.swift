f056f45cb2ba9c00222d3107c1b5e2e2f9169aa5
hanni66 <haeunkim0807@naver.com>
2022-08-20 13:51:43 +0900

    Feat : firstView Text 추가

--- Weather_Alarm_iOS/Weather_Alarm_iOS/VC/FirstViewController.swift
+++ Weather_Alarm_iOS/Weather_Alarm_iOS/VC/FirstViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    // Weather-Alarm
+    lazy var mainText: UILabel = {
+        let mainText = UILabel()
+        mainText.textColor = .black
+        mainText.font = UIFont(name: "NotoSansKR-Bold", size: 20)
+        mainText.font = UIFont.systemFont(ofSize: 30)
+        mainText.text = "Weather-Alarm First View"
+        mainText.sizeToFit()
+        return mainText
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.addSubview(self.mainText)
     }
 
 
