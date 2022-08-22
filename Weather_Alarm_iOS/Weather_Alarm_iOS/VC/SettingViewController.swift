ebed147d9bf212fe54f200b7ff8cd06733d0fcc9
hanni66 <haeunkim0807@naver.com>
2022-08-22 12:33:21 +0900

    Feat : Btn 추가, 데이트 피커 추가

--- Weather_Alarm_iOS/Weather_Alarm_iOS/VC/SettingViewController.swift
+++ Weather_Alarm_iOS/Weather_Alarm_iOS/VC/SettingViewController.swift
@@ -8,23 +8,81 @@
 import UIKit
 
 class SettingViewController: UIViewController {
-
+    
     // Weather-Alarm
     lazy var mainText: UILabel = {
         let mainText = UILabel()
         mainText.textColor = .black
-        mainText.font = UIFont(name: "NotoSansKR-Bold", size: 20)
+        mainText.font = UIFont(name: "설정", size: 20)
         mainText.font = UIFont.systemFont(ofSize: 30)
         mainText.text = "Weather-Alarm Setting View"
         mainText.sizeToFit()
         return mainText
     }()
     
+    // 데이트피커 사용
+    lazy var DateTime: UILabel = {
+        let dateTime = UILabel()
+        dateTime.textColor = .black
+        dateTime.font = UIFont(name: "현재시간", size: 20)
+        dateTime.font = UIFont.systemFont(ofSize: 30)
+        dateTime.text = "Weather-Alarm Setting View"
+        dateTime.sizeToFit()
+        return dateTime
+    }() // 현재 시간을 표시할 라벨
+    
+    lazy var PickerTime: UILabel = {
+        let pickerTime = UILabel()
+        pickerTime.textColor = .black
+        pickerTime.font = UIFont(name: "선택 시간", size: 20)
+        pickerTime.font = UIFont.systemFont(ofSize: 30)
+        pickerTime.text = "Weather-Alarm Setting View"
+        pickerTime.sizeToFit()
+        return pickerTime
+    }() // 선택 시간을 표시할 라벨
+    
+    var isClickConfirm = false // 얼럿 확인 버튼이 눌렸는지 저장할 변수
+    
+    @IBAction func DatePicker(_ sender: UIDatePicker) {
+        let formatter = DateFormatter() // 특정 포맷으로 날짜를 보여주기 위한 변수 선언
+        formatter.dateFormat = "yyyy-MM-dd HH:mm" // 날짜 포맷 지정
+        PickerTime.text = formatter.string(from: sender.date) // 선택 시간 라벨에 지정한 날짜 포맷으로 입력
+    }
+    
+    @objc func timerOn() {
+        isClickConfirm = false // 초기화
+    }
+    
+    @objc func updatetime() {
+       let formatter = DateFormatter() // 특정 포맷으로 날짜를 보여주기 위한 변수 선언
+       formatter.dateFormat = "yyyy-MM-dd HH:mm" // 날짜 포맷 지정
+       DateTime.text = formatter.string(from: Date()) // 현재 시간 라벨에 지정한 날짜 포맷으로 입력
+       
+       if isClickConfirm { // 얼럿 확인 버튼이 눌렸으면(true), 종료
+           return
+       }
+       
+       if PickerTime.text == DateTime.text { // 선택 시간과 현재 시간이 같으면
+          self.isClickConfirm = true // 얼럿 확인 버튼을 누르면, true 로 변경
+
+          // 60초 후에 timerOn 함수를 실행시키는 타이머
+          Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(self.timerOn), userInfo: nil, repeats: false)
+           // 얼럿창 설정
+           let alert = UIAlertController(title: "알림", message: "설정된 시간입니다 !!!", preferredStyle: .alert)
+           alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default) { UIAlertAction in
+           })
+           self.present(alert, animated: true, completion: nil) // 얼럿 실행
+       }
+   }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.view.addSubview(mainText)
+        
+        // 1초마다 updatetime함수를 실행시키는 타이머
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updatetime), userInfo: nil, repeats: true)
     }
 
 
--- Weather_Alarm_iOS/Weather_Alarm_iOS/VC/ViewController.swift
+++ Weather_Alarm_iOS/Weather_Alarm_iOS/VC/ViewController.swift
@@ -15,31 +15,31 @@ class ViewController: UIViewController {
         mainText.textColor = .black
         mainText.font = UIFont(name: "NotoSansKR-Bold", size: 20)
         mainText.font = UIFont.systemFont(ofSize: 30)
-        mainText.text = "Weather-Alarm"
+        mainText.text = "알람"
         mainText.sizeToFit()
         return mainText
     }()
     
     // 버튼
-    lazy var linkConnectionBtn: UIButton = {
-        let connectionBtn = UIButton()
-        connectionBtn.setTitle("더 알아보기", for: .normal)     // 버튼에 들어갈 글씨
-        connectionBtn.backgroundColor = .white       // 버튼 색상
-//        connectionBtn.setTitleColor(.darkGreen, for: .normal) // 버튼 글씨 색상
-        connectionBtn.layer.cornerRadius = 5
-        connectionBtn.layer.borderWidth = 1
-//        connectionBtn.layer.borderColor = UIColor.darkGreen?.cgColor
-        connectionBtn.layer.cornerRadius = (15)
-        connectionBtn.translatesAutoresizingMaskIntoConstraints = false
-//        connectionBtn.addTarget(self, action: #selector(connectionAction), for: .touchUpInside)
-        return connectionBtn
+    lazy var settingBtn: UIButton = {
+        let settingBtn = UIButton()
+        settingBtn.setTitle("세팅view로 가는 버튼", for: .normal)     // 버튼에 들어갈 글씨
+        settingBtn.backgroundColor = .white       // 버튼 색상
+//        settingBtn.setTitleColor(.darkGreen, for: .normal) // 버튼 글씨 색상
+        settingBtn.layer.cornerRadius = 5
+        settingBtn.layer.borderWidth = 1
+//        settingBtn.layer.borderColor = UIColor.darkGreen?.cgColor
+        settingBtn.layer.cornerRadius = (15)
+        settingBtn.translatesAutoresizingMaskIntoConstraints = false
+//        settingBtn.addTarget(self, action: #selector(connectionAction), for: .touchUpInside)
+        return settingBtn
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.addSubview(self.mainText)
-        self.view.addSubview(self.linkConnectionBtn)
+        self.view.addSubview(self.settingBtn)
     }
 
 
